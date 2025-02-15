7a0f6e59ce05baf25ee613c475a24b3d6dfcd1ea
Yihong Li <154848136+yihongli-04@users.noreply.github.com>
2025-02-14 23:33:54 -0800

    Set up node

---
+++ backend/index.js
@@ -0,0 +1,14 @@
+const { Sequelize } = require('sequelize')
+const env = require('dot-env')
+
+console.log(env.)
+
+// const sequelize = Sequelize()
+
+// const initDB = async () => {
+//     try {
+//         await Sequeli
+//     } catch (error) {
+
+//     }
+// }
